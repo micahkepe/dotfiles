606e53c030b2b53e2c0f68c1a19937d609e4fa42
micahkepe <micahkepe@gmail.com>
2023-08-15 21:29:34 -0500

    Add install/bootstrap script to automate symlinks

---
+++ bootstrap.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+DOTFILES_DIR=~/.dotfiles
+
+# Symlink function
+symlink() {
+    local src=$1 dst=$2
+
+    # If the file already exists, skip it
+    if [ -e "$dst" ]; then
+        echo "File $dst already exists. Skipping..."
+    else
+        echo "Linking $src to $dst"
+        ln -s $src $dst
+    fi
+}
+
+# Link configs
+symlink $DOTFILES_DIR/.bashrc ~/.bashrc
+symlink $DOTFILES_DIR/.gitconfig ~/.gitconfig
+symlink $DOTFILES_DIR/.vimrc ~/.vimrc
+symlink $DOTFILES_DIR/.zshrc ~/.zshrc
+symlink $DOTFILES_DIR/.hammerspoon ~/.hammerspoon
+
+echo "Dotfiles linked successfully!"
