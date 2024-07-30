fee754f6168387f6fef17ba5d3284c488450afa2
micahkepe <micahkepe@gmail.com>
2024-07-30 14:47:47 -0700

    Add mapping to escape terminal mode

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -8,3 +8,6 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- Map <ESC> to exit terminal mode
+map("t", "<ESC>", "<C-\\><C-n>")
