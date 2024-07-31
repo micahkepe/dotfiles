54895d761d469075d68a2d852f03c67f21e9664c
micahkepe <micahkepe@gmail.com>
2024-07-30 19:38:04 -0700

    Set default branch to main, enable Copilot on dotfiles (- .env)

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -27,6 +27,14 @@ return {
       filetypes = {
         markdown = true,
         help = true,
+        ["."] = true,
+        sh = function()
+          if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), "^%.env.*") then
+            -- disable for .env files
+            return false
+          end
+          return true
+        end,
       },
     },
     config = function(_, opts)
