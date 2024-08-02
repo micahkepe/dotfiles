1c1639b8bc6b0c3daa79ca71f5b6d4729a0af92f
micahkepe <micahkepe@gmail.com>
2024-08-02 13:51:26 -0700

    Override comments to be italicized

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -1,20 +1,20 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
 local M = {}
 
 M.ui = {
-	theme = "onedark",
+  theme = "onedark",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
- 
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
+
   nvdash = {
-    load_on_startup = true,  
-    
+    load_on_startup = true,
+
     header = {
       [[███╗   ██╗██╗   ██╗██╗███╗   ███╗]],
       [[████╗  ██║██║   ██║██║████╗ ████║]],
@@ -22,9 +22,9 @@ M.ui = {
       [[██║╚██╗██║╚██╗ ██╔╝██║██║╚██╔╝██║]],
       [[██║ ╚████║ ╚████╔╝ ██║██║ ╚═╝ ██║]],
       [[╚═╝  ╚═══╝  ╚═══╝  ╚═╝╚═╝     ╚═╝]],
-      [[                                 ]]
+      [[                                 ]],
     },
-  }
+  },
 }
 
 return M
