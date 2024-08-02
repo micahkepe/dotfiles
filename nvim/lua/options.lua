f274a7149da46efda09a2a2d1f6839756b599829
micahkepe <micahkepe@gmail.com>
2024-08-02 13:14:24 -0700

    Search and replace opts

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -3,4 +3,16 @@ require "nvchad.options"
 -- add yours here!
 
 local o = vim.o
-o.cursorlineopt ='both' -- to enable cursorline!
+local opt = vim.opt
+
+-- Cursorline
+o.cursorlineopt = "both" -- to enable cursorline!
+
+-- Make line numbers defaults
+o.number = true
+
+-- Search and replace
+opt.ignorecase = true -- ignore case letters when searching
+opt.smartcase = true -- match case if capital letter is present
+opt.incsearch = true -- show search matches as you type
+opt.inccommand = "split" -- show live preview of substitute commands
