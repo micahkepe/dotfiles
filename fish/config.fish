88baeaaa8dcc7900f60a1e86a611686c066d1323
micahkepe <micahkepe@gmail.com>
2024-09-24 09:45:33 -0500

    ssh alias to use Kitty conf when in Kitty emulator

--- fish/config.fish
+++ fish/config.fish
@@ -19,3 +19,12 @@ alias gs="git status"
 alias ga="git add"
 alias gc="git commit"
 alias gp="git push"
+
+# Using the Fish shell in SSH w/ Kitty terminal emulator
+#
+# DO NOT SET `TERM` to a value other than the Kitty default:
+#   https://sw.kovidgoyal.net/kitty/conf/#opt-kitty.term
+if test -n "$KITTY_WINDOW_ID"
+    # Alias for ssh to use kitty terminal emulator
+    alias ssh="kitty +kitten ssh"
+end
