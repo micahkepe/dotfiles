1d98fc607c9f7f69073919da4de2521d01a24f0c
micahkepe <micahkepe@gmail.com>
2024-10-15 15:43:39 -0500

    tmux passthrough, remove unused wilder setup code

--- nvim/lua/plugins/wilder.lua
+++ nvim/lua/plugins/wilder.lua
@@ -10,24 +10,6 @@ return {
       local wilder = require "wilder"
       wilder.setup { modes = { ":" } }
 
-      -- wilder.set_option("pipeline", {
-      --   wilder.branch(
-      --     wilder.python_file_finder_pipeline {
-      --       file_command = function(_, arg)
-      --         if string.find(arg, ".") ~= nil then
-      --           return { "fd", "-tf", "-H" }
-      --         else
-      --           return { "fd", "-tf" }
-      --         end
-      --       end,
-      --       dir_command = { "fd", "-td" },
-      --       filters = { "fuzzy_filter", "difflib_sorter" },
-      --     },
-      --     wilder.cmdline_pipeline(),
-      --     wilder.python_search_pipeline()
-      --   ),
-      -- })
-
       wilder.set_option(
         "renderer",
         wilder.popupmenu_renderer {
