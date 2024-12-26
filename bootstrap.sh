c6765218e0e72558b5802f37b3f9012169f27c80
micahkepe <micahkepe@gmail.com>
2024-12-26 15:16:26 -0800

    Add Ghostty, update bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,41 @@
 #!/bin/bash
 
+# DESCRIPTION : This script symlinks all the dotfiles to the home directory. In 
+# the future more complex bootstrap functionality could be added to install 
+# packages, set up the environment, etc. This script assumes that the dotfiles 
+# dotfiles are stored in a directory called .dotfiles in the home directory. 
+# AUTHOR      : Micah Kepe 
+# DATE        : 2024-12-26
+
+### Prerequsite checks
+
+# Check if git is installed 
+if ! command -v git &> /dev/null; then
+    echo "Git is not installed. Please install git and try again."
+    exit 1
+fi
+
+# Check if brew is installed 
+if ! command -v brew &> /dev/null; then 
+   echo "Brew is not installed. Please install brew and try again."
+   exit 1 
+fi
+
+### Main script
+
+if [ ! -d ~/.dotfiles ]; then
+    echo "Cloning dotfiles..."
+    git clone https://github.com/micahkepe/dotfiles 
+fi 
+
+# Set the dotfiles directory 
 DOTFILES_DIR=~/.dotfiles
 
-# Symlink function
+# try installing all of the packages int eh Brewfile bundle 
+echo "Installing packages from Brewfile..."
+brew bundle --file=~/.dotfiles/Brewfile
+
+# Symlink helper function
 symlink() {
     local src=$1 dst=$2
 
@@ -15,7 +48,8 @@ symlink() {
     fi
 }
 
-# Link configs
+# symlink configs 
+echo "Linking dotfiles..."
 symlink $DOTFILES_DIR/.bashrc ~/.bashrc
 symlink $DOTFILES_DIR/.gitconfig ~/.gitconfig
 symlink $DOTFILES_DIR/.vimrc ~/.vimrc
@@ -29,6 +63,7 @@ symlink $DOTFILES_DIR/tmux/.tmux.conf ~/.tmux.conf
 symlink $DOTFILES_DIR/wezterm ~/.config/wezterm 
 symlink ~/.wezterm.lua $DOTFILES_DIR/wezterm/wezterm.lua
 symlink $DOTFILES_DIR/spotify-player ~/.config/spotify-player
+symlink $DOTFILES_DIR/ghostty ~/.config/ghostty
 # add more here as needed
 
 echo "Dotfiles linked successfully!"
