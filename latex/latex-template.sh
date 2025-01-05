7b4022e3467f6209c4cdbaaf6c681a58d064a0ad
micahkepe <micahkepe@gmail.com>
2025-01-04 19:19:49 -0800

    Add more colorful completion menu, strip more whitespace

--- latex/latex-template.sh
+++ latex/latex-template.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-# Description: 
-#   Copy the contents of ~/.dotfiles/latex/*.tex to the current directory 
+# Description:
+#   Copy the contents of ~/.dotfiles/latex/*.tex to the current directory
 #
 # Usage:
-#  latex-template.sh [directory] 
+#  latex-template.sh [directory]
 #
-#  If no directory is passed in, the script will copy the contents of the 
+#  If no directory is passed in, the script will copy the contents of the
 #  file passed in to the current directory.
 
 if [[ $# -eq 1 ]]; then
     dir=$1
 else
     dir=$(pwd)
-fi 
+fi
 
-cp ~/.dotfiles/latex/*.tex $dir 
+cp ~/.dotfiles/latex/*.tex $dir
 
