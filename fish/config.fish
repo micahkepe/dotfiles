c8f9e687727cc174e2dcb61d2a4979621be8b5cc
micahkepe <micahkepe@gmail.com>
2025-03-05 11:41:25 -0600

    chore(Brewfile): update brew bundle, add Yazi functions and set nvim as default editor system wide

--- fish/config.fish
+++ fish/config.fish
@@ -58,3 +58,15 @@ zoxide init --cmd cd fish | source
 
 # pyenv setup
 pyenv init - fish | source
+
+# Yazi
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
+export EDITOR="/opt/homebrew/bin/nvim"
