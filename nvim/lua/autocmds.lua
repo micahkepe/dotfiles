92dbbed1d38e476fdee234616ece984b050710c8
micahkepe <micahkepe@gmail.com>
2025-07-01 18:47:05 -0700

    feat(tmux): spotify_player popup mapping; yank highlight autocmd

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -12,3 +12,11 @@ autocmd("CursorHold", {
     vim.diagnostic.open_float(nil, { focus = false })
   end,
 })
+
+autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
