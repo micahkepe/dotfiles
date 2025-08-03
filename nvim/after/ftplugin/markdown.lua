f2990e4665687b32d2b4b1c0066e06a429819f1e
micahkepe <micahkepe@gmail.com>
2025-08-03 11:57:46 -0700

    ftplugin(markdown): ToC Markdown keymap

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -1,2 +1,9 @@
+local map = vim.keymap.set
+
 -- Set text width to 80 columns
 vim.bo.textwidth = 80
+
+-- Generate table of contents with `gh-md-toc`
+map("n", "<leader>toc", ":0read !gh-md-toc --hide-footer %<CR>", {
+  desc = "Insert Table of Contents for the current Markdown file at first line",
+})
