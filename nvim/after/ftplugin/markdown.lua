5e219ce1b3b39285bbe11132ef50d9ba55617983
micahkepe <micahkepe@gmail.com>
2025-08-17 15:57:02 -0700

    update dry-run option for bootstrap script, misc. updates

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -2,8 +2,12 @@ local map = vim.keymap.set
 
 -- Set text width to 80 columns
 vim.bo.textwidth = 80
+-- Update tabwidth to 2 spaces
+vim.bo.tabstop = 2
+vim.bo.shiftwidth = 2
+vim.bo.expandtab = true
 
 -- Generate table of contents with `gh-md-toc`
-map("n", "<leader>toc", ":0read !gh-md-toc --hide-footer %<CR>", {
+map("n", "<leader>toc", ":0read !gh-md-toc --hide-header --hide-footer%<CR>", {
   desc = "Insert Table of Contents for the current Markdown file at first line",
 })
