16456925079440eb76ff780a77aa6215edcde3ff
micahkepe <micahkepe@gmail.com>
2025-08-27 16:34:59 -0500

    feat(tmux): prettier options for session-fzf tmux aux. script

--- tmux/session-fzf.sh
+++ tmux/session-fzf.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-tmux list-sessions | sed -E 's/:.*$//' | grep -v \"^"$(tmux display-message -p '#S')"\$\" | fzf --reverse | xargs tmux switch-client -t
+tmux list-sessions |
+  sed -E 's/:.*$//' |
+  grep -v \"^"$(tmux display-message -p '#S')"\$\" |
+  fzf --reverse --ghost="Session name" --header="Available Sessions:" --height=10 --border --border=rounded --border-label=" Switch Tmux Session " --color=label:italic:black |
+  xargs tmux switch-client -t
