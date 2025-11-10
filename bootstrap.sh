a82df198f18cdf6aaea4f2bc88372af8a4d441fe
micahkepe <micahkepe@gmail.com>
2025-11-10 11:03:40 -0600

    fix(tmux-sessionizer): allow `-mindepth 0` for root folders
    - In bootstrap script, symlink over copying local scripts to reduce code
      duplication and make changes to script reflect system-wide without
      re-copying

--- bootstrap.sh
+++ bootstrap.sh
@@ -219,9 +219,9 @@ exec-dry-run mkdir -p "$LOCAL_BIN"
 exec-dry-run chmod +x "$DOTFILES_DIR"/tmux/tmux-sessionizer.sh
 exec-dry-run chmod +x "$DOTFILES_DIR"/tmux/session-fzf.sh
 exec-dry-run chmod +x "$DOTFILES_DIR"/tmux/is-vim.sh
-exec-dry-run cp -f "$DOTFILES_DIR"/tmux/tmux-sessionizer.sh "$HOME"/.local/bin/tmux-sessionizer
+exec-dry-run symlink "$DOTFILES_DIR"/tmux/tmux-sessionizer.sh "$HOME"/.local/bin/tmux-sessionizer
 exec-dry-run chmod +x "$DOTFILES_DIR"/latex/latex-template.sh
-exec-dry-run cp -f "$DOTFILES_DIR"/latex/latex-template.sh "$HOME"/.local/bin/latex-template
+exec-dry-run symlink "$DOTFILES_DIR"/latex/latex-template.sh "$HOME"/.local/bin/latex-template
 echo "Local scripts made executeable!"
 
 ## vim-plug
