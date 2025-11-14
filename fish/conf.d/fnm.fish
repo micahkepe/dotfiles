df862739f84ff9210e8961101fa6280a8a91e092
micahkepe <micahkepe@gmail.com>
2025-11-13 21:10:44 -0600

    fix(fnm): properly source fnm variables
    See: https://github.com/Schniz/fnm?tab=readme-ov-file#fish-shell

--- fish/conf.d/fnm.fish
+++ fish/conf.d/fnm.fish
@@ -1,6 +1,1 @@
-# fnm
-set FNM_PATH "/Users/micahkepe/Library/Application Support/fnm"
-if [ -d "$FNM_PATH" ]
-  set PATH "$FNM_PATH" $PATH
-  fnm env | source
-end
+fnm env --shell fish | source
--- fish/config.fish
+++ fish/config.fish
@@ -6,7 +6,6 @@ set -gx PATH \
     $HOME/.pyenv/bin \
     /Library/TeX/texbin \
     $HOME/.rvm/bin \
-    "/Users/micahkepe/.local/state/fnm_multishells/97325_1729710187621/bin" \
     $HOME/.cargo/bin \
     $HOME/.local/bin \
     $PATH
@@ -87,17 +86,6 @@ end
 if test -n "$KITTY_WINDOW_ID"
     function ssh; kitty +kitten ssh $argv; end
 end
-
-# FNM ENV SETUP
-set -gx PATH "/Users/micahkepe/.local/state/fnm_multishells/97325_1729710187621/bin" $PATH
-set -gx FNM_MULTISHELL_PATH "/Users/micahkepe/.local/state/fnm_multishells/97325_1729710187621"
-set -gx FNM_VERSION_FILE_STRATEGY "local"
-set -gx FNM_DIR "/Users/micahkepe/.local/share/fnm"
-set -gx FNM_LOGLEVEL "info"
-set -gx FNM_NODE_DIST_MIRROR "https://nodejs.org/dist"
-set -gx FNM_COREPACK_ENABLED "false"
-set -gx FNM_RESOLVE_ENGINES "false"
-set -gx FNM_ARCH "arm64"
 
 # SET UP FZF KEY BINDINGS
 fzf --fish | source
