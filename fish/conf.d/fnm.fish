b78e4833e827f096824bd6992b465590b7a220e4
micahkepe <micahkepe@gmail.com>
2025-11-22 14:53:46 -0600

    feat(fnm): make fish init crossplatform with Linux

--- fish/conf.d/fnm.fish
+++ fish/conf.d/fnm.fish
@@ -1,1 +1,10 @@
-fnm env --shell fish | source
+# fnm
+if uname -a | grep -q "Darwin";
+  set FNM_PATH "/opt/homebrew/opt/fnm/bin"
+else
+  set FNM_PATH "$HOME/.local/share/fnm"
+end
+
+if [ -d "$FNM_PATH" ]
+  fnm env --shell fish | source
+end
