ef79cdcefbf92f90360c3f4a8722491f821645ff
Micah Kepe <micahkepe@gmail.com>
2026-02-28 17:12:27 -0800

    refactor(tmux): use native `fzf --tmux` option

--- tmux/session-fzf.sh
+++ tmux/session-fzf.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
-tmux list-sessions |
-  sed -E 's/:.*$//' |
-  grep -v \"^"$(tmux display-message -p '#S')"\$\" |
-  fzf --reverse --ghost="Session name" --header="Available Sessions:" --height=10 --border --border=rounded --border-label=" Switch Tmux Session " --color=label:italic:black |
-  xargs tmux switch-client -t
+#!/usr/bin/env bash
+
+current="$(tmux display-message -p '#S')"
+
+tmux list-sessions -F '#S' |
+  grep -v "^$current$" |
+  fzf --tmux --reverse |
+  xargs -r -I{} tmux switch-client -t "{}"
