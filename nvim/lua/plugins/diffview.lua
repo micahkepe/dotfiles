2f5b81443fec648cc29cc204622d6bd478cf716e
Micah Kepe <micahkepe@gmail.com>
2026-08-27 09:19:41 -0500

    chore(diffview): show winbar info

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -13,6 +13,12 @@ return {
       },
       enhanced_diff_hl = true,
       view = {
+        default = {
+          winbar_info = true,
+        },
+        file_history = {
+          winbar_info = true,
+        },
         merge_tool = {
           layout = "diff4_mixed",
           disable_diagnostics = true,
