f35b22ac82a4490882d3c3bc17b725c8e245a38a
Micah Kepe <micahkepe@gmail.com>
2026-09-16 14:54:09 -0500

    chore: remove targetDir

--- nvim/lua/plugins/rustaceanvim.lua
+++ nvim/lua/plugins/rustaceanvim.lua
@@ -13,7 +13,6 @@ return {
               features = "all",
               -- Uncomment for when working with WASM
               -- target = "wasm32-unknown-unknown",
-              targetDir = true,
             },
             check = {
               command = "clippy",
