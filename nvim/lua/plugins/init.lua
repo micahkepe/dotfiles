f947e776feb8c4949e9d4b69acf528f1c93d7faa
Micah Kepe <micahkepe@gmail.com>
2026-09-25 11:28:35 -0500

    chore(oil): show full -lah equivalent info

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -274,7 +274,14 @@ return {
 
   {
     "stevearc/oil.nvim",
-    opts = {},
+    opts = {
+      columns = {
+        "icon",
+        "permissions",
+        "size",
+        "mtime",
+      },
+    },
     dependencies = { "nvim-tree/nvim-web-devicons" },
     lazy = false,
   },
